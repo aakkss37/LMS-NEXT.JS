6deb3a489bc36c5778b3e89d91a5b13cbf127f77
amar kumar <amarkumar.sharma.124@gmail.com>
2024-03-20 11:50:42 +0530

    First commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+	return (
+		<main className="">
+			<Button>Click me</Button>
+			this is  a protected page
+		</main>
+	);
+}
